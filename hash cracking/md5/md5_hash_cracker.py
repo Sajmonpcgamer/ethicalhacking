9711f3ab3518a5c436afb382a5989ff1449f56e1
Sajmonpcgamer <80468592+Sajmonpcgamer@users.noreply.github.com>
2022-08-08 10:35:11 +0200

    Create md5_hash_cracker.py

---
+++ hash cracking/md5/md5_hash_cracker.py
@@ -0,0 +1,63 @@
+# Code written by https://github.com/Sajmonpcgamer
+
+import os.path
+import hashlib
+import string
+import random
+from os import getcwd
+from sys import argv as args
+from sys import platform as SYSPLAT
+
+if "--random" in str(args) or "-r" in args:
+    if len(args) < 3:
+        exit("Hash and max length needs to be specified")
+    elif args[1] == "--random" or args[2] == "--random":
+        exit("python3 md5_hash_cracker.py <hash> <max_length> --random")
+    h = args[1].encode()
+    m_l = int(args[2])
+    h_t = None
+    h_c = string.ascii_letters + string.digits + string.punctuation + string.whitespace
+
+    print("==============================\nmd5 hash cracker\nWritten by Sajmonpcgamer\nhttps://github.com/Sajmonpcgamer\n==============================\n")
+    print("!Warning this is a BETA feature, only hex works!")
+
+    while h_t != h:
+        h_t = hashlib.md5(str(random.choice(h_c) for i in range(random.randint(1, m_l))).encode()).hexdigest().encode()
+    exit("Hash cracked!\nCracked hash: " + h_t)
+else:
+    if len(args) < 3:
+        exit("python3 md5_hash_cracker.py <list> <hash>\nOptions:\n\t--hex = enables HEX mode\n\t--random uses random strings")
+    filename = args[1]
+    if not "/" in filename or not "\\" in filename:
+        if "win" in SYSPLAT:
+            filename = getcwd() + "\\" + str(args[1])
+        else:
+            filename = getcwd() + "/" + str(args[1])
+    if not os.path.exists(filename):
+        exit("This list does not exist!")
+    if not os.path.isfile(filename):
+        exit("This is not a file!")
+
+    print("==============================\nmd5 hash cracker\nWritten by Sajmonpcgamer\nhttps://github.com/Sajmonpcgamer\n==============================\n")
+
+    h = args[2].encode()
+
+    if "--hex" in str(args) or "-h" in str(args):
+        print("Hex Mode enabled!")
+
+    words = []
+    with open(filename, "r") as l:
+        words = l.readlines()
+
+    for i in words:
+        w = hashlib.md5(i.replace("\n", "").encode())
+        if "--hex" in str(args) or "-h" in str(args):
+            w = w.hexdigest().encode()
+        else:
+            w = w.digest()
+        
+        if w == h:
+            exit("Hash cracked!\nCracked hash: " + i)
+
+    exit("Hash was not found :(")
+exit()
