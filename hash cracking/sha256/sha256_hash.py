46cb1634334d9ef2e6a79802a7cd4434d30ca5d9
Sajmonpcgamer <80468592+Sajmonpcgamer@users.noreply.github.com>
2022-08-08 11:43:55 +0200

    Update sha256_hash.py

--- hash cracking/sha256/sha256_hash.py
+++ hash cracking/sha256/sha256_hash.py
@@ -1,8 +1,8 @@
 # Code written by https://github.com/Sajmonpcgamer
 import hashlib
 
-print("==============================\nmd5 hash creator\nWritten by Sajmonpcgamer\nhttps://github.com/Sajmonpcgamer\n==============================\n")
+print("==============================\nsha256 hash creator\nWritten by Sajmonpcgamer\nhttps://github.com/Sajmonpcgamer\n==============================\n")
 
 h = str(input("Word to be hashed: ")).encode()
-h = hashlib.md5(h).hexdigest()
+h = hashlib.sha256(h).hexdigest()
 print(f"Hash (Hex): {h}")
